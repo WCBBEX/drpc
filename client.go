93cb18e1e2197df6e3fedca4a864b774f123ed37
WCBBEX <354127216@qq.com>
2026-06-06 20:46:33 +0800

    客户端

---
+++ client.go
@@ -0,0 +1,220 @@
+package drpc
+
+import (
+	"drpc/codec"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"sync/atomic"
+)
+
+type Client struct {
+	mu       sync.Mutex
+	codec    codec.Codec
+	opt      *Option
+	sending  sync.Mutex
+	header   codec.Header
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  atomic.Bool
+	shutdown atomic.Bool
+}
+
+func NewClient(conn net.Conn, opt *Option) (*Client, error) {
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		err := fmt.Errorf("invalid codec type %s", opt.CodecType)
+		log.Println("rpc client: codec error:", err)
+		return nil, err
+	}
+
+	if err := json.NewEncoder(conn).Encode(opt); err != nil {
+		log.Println("rpc client: options error: ", err)
+		_ = conn.Close()
+		return nil, err
+	}
+
+	client := &Client{
+		seq:     1,
+		codec:   f(conn),
+		opt:     opt,
+		pending: make(map[uint64]*Call),
+	}
+
+	go client.receive()
+	return client, nil
+}
+
+func (c *Client) Close() error {
+	if c.closing.Swap(true) {
+		return ErrShutdown
+	}
+
+	return c.codec.Close()
+}
+
+func (c *Client) isAvailable() bool {
+	return !c.closing.Load() && !c.shutdown.Load()
+}
+
+func (c *Client) registerCall(call *Call) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.closing.Load() || c.shutdown.Load() {
+		return 0, ErrShutdown
+	}
+
+	call.Seq = c.seq
+	c.pending[call.Seq] = call
+	c.seq++
+
+	return call.Seq, nil
+}
+
+func (c *Client) removeCall(seq uint64) *Call {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	call := c.pending[seq]
+	delete(c.pending, seq)
+	return call
+}
+
+func (c *Client) terminateCalls(err error) {
+	c.sending.Lock()
+	defer c.sending.Unlock()
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.shutdown.Store(true)
+	for _, call := range c.pending {
+		call.Error = err
+		call.done()
+	}
+}
+
+func (c *Client) receive() {
+	var err error
+	for err == nil {
+		var h codec.Header
+		if err = c.codec.ReadHeader(&h); err != nil {
+			break
+		}
+
+		call := c.removeCall(h.Seq)
+		switch {
+		case call == nil:
+			err = c.codec.ReadBody(nil)
+		case h.Error != "":
+			call.Error = fmt.Errorf(h.Error)
+			err = c.codec.ReadBody(nil)
+			call.done()
+		default:
+			err = c.codec.ReadBody(call.Reply)
+			if err != nil {
+				call.Error = errors.New("reading body " + err.Error())
+			}
+			call.done()
+		}
+	}
+
+	c.terminateCalls(err)
+}
+
+func (c *Client) send(call *Call) {
+	c.sending.Lock()
+	defer c.sending.Unlock()
+
+	seq, err := c.registerCall(call)
+	if err != nil {
+		call.Error = err
+		call.done()
+		return
+	}
+
+	c.header.ServiceMethod = call.ServiceMethod
+	c.header.Seq = seq
+	c.header.Error = ""
+
+	if err := c.codec.Write(&c.header, call.Args); err != nil {
+		call := c.removeCall(seq)
+		if call != nil {
+			call.Error = err
+			call.done()
+		}
+	}
+}
+
+func (c *Client) Go(serviceMethod string, args, reply any, done chan *Call) *Call {
+	if done == nil {
+		done = make(chan *Call, 10)
+	} else if cap(done) == 0 {
+		log.Panic("rpc client: done channel is unbuffered")
+	}
+
+	call := &Call{
+		ServiceMethod: serviceMethod,
+		Args:          args,
+		Reply:         reply,
+		Done:          done,
+	}
+	c.send(call)
+	return call
+}
+
+func (c *Client) Call(serviceMethod string, args, reply any) error {
+	call := <-c.Go(serviceMethod, args, reply, make(chan *Call, 1)).Done
+	return call.Error
+}
+
+var ErrShutdown = errors.New("connection is shut down")
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          any
+	Reply         any
+	Error         error
+	Done          chan *Call
+}
+
+func (c *Call) done() {
+	c.Done <- c
+}
+
+func parseOptions(opts ...*Option) (*Option, error) {
+	if len(opts) == 0 || opts[0] == nil {
+		return DefaultOption, nil
+	}
+
+	if len(opts) != 1 {
+		return nil, errors.New("number of options is more than 1")
+	}
+
+	return opts[0], nil
+}
+
+func Dial(network, address string, opts ...*Option) (client *Client, err error) {
+	opt, err := parseOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if client == nil {
+			_ = conn.Close()
+		}
+	}()
+
+	return NewClient(conn, opt)
+}
